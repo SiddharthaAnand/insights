24bd330e63998756e541bfc32ee1b2be758cf9da
SiddharthaAnand <siddharthalibra13@gmail.com>
2018-02-24 17:55:55 +0530

    remove debug statement

--- app.py
+++ app.py
@@ -68,6 +68,8 @@ def sign_s3():
 
 
 if __name__ == '__main__':
-    app.debug = True
+    '''app.debug = True
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    '''
+    app.run()
