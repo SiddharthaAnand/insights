e772d8c401d67596ab2344fa88fc2ec860d33182
SiddharthaAnand <siddharthalibra13@gmail.com>
2018-02-24 13:20:59 +0530

    Add file upload functionality

--- app.py
+++ app.py
@@ -1,6 +1,46 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "../tmp/"
+ALLOWED_EXTENSIONS = set(["txt"])
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER}"] = UPLOAD_FOLDER
+
+
+def allowed_filename(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        if 'file' in request.files:
+            flash('no file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_filename(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename ))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>'''
 
 
 @app.route("/")
